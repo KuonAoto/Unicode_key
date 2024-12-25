72f82175e83b8b83bd14f4cd0c3dc75348811751
KuonAoto <aoto.kuon@gmail.com>
2024-12-25 19:12:26 +0900

    Windowsサービスをば

---
+++ UnicodeKeyboardInputService/UnicodeKeyboardInputService/ProjectInstaller.Designer.cs
@@ -0,0 +1,59 @@
+﻿namespace UnicodeKeyboardInputService
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.serviceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.serviceInstaller1 = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // serviceProcessInstaller
+            // 
+            this.serviceProcessInstaller.Password = null;
+            this.serviceProcessInstaller.Username = null;
+            // 
+            // serviceInstaller1
+            // 
+            this.serviceInstaller1.Description = "DirectUnicodeInput";
+            this.serviceInstaller1.DisplayName = "UniKeyInput";
+            this.serviceInstaller1.ServiceName = "UnicodeInputService";
+            this.serviceInstaller1.StartType = System.ServiceProcess.ServiceStartMode.Automatic;
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.serviceProcessInstaller,
+            this.serviceInstaller1});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller serviceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller serviceInstaller1;
+    }
+}
---
+++ UnicodeKeyboardInputService/UnicodeKeyboardInputService/UnicodeInputService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnicodeKeyboardInputService
+{
+    public partial class UnicodeInputService : ServiceBase
+    {
+        public UnicodeInputService()
+        {
+            InitializeComponent();
+
+            //イベントログ表示
+            ServiceEventLog = new System.Diagnostics.EventLog();
+            if (!System.Diagnostics.EventLog.SourceExists("ServiceTest"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+                    "ServiceTest", "Application");
+            }
+            ServiceEventLog.Source = "ServiceTest";
+            ServiceEventLog.Log = "Application";
+
+        }
+
+        protected override void OnContinue()
+        {
+
+            ServiceEventLog.WriteEntry("OnContinue Called.");
+            base.OnContinue();
+        }
+        protected override void OnPause()
+        {
+            ServiceEventLog.WriteEntry("OnPause Called.");
+            base.OnPause();
+        }
+        protected override void OnShutdown()
+        {
+            base.OnShutdown();
+        }
+        protected override bool OnPowerEvent(PowerBroadcastStatus powerStatus)
+        {
+            return base.OnPowerEvent(powerStatus);
+        }
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
